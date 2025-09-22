2ed29b27fae2dd869f8b03bce053f1664fc981db
JacksonsPenn <93682236+JacksonsPenn@users.noreply.github.com>
2025-09-21 20:07:54 -0400

    Changed it

---
+++ src/Shared/Models/Notebook/NotebookSection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace Common.Models.Notebook;
+
+public class NotebookSection
+{
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+    public string Content { get; set; } // Markdown or plain text
+    public string Output { get; set; } // Output for the section
+    public OutputPosition Position { get; set; } = OutputPosition.After;
+}
+
+public enum OutputPosition
+{
+    Before,
+    After
+}
